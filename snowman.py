d9aad964e99f66c3c65cf2ca4985155d44aa1c48
fabiomorena <fabio@morenamail.de>
2025-06-29 10:29:13 +0200

    Improved Final Code

--- snowman.py
+++ snowman.py
@@ -1,5 +1,16 @@
 from game_logic import play_game
 
-if __name__ == "__main__":
-    play_game()
 
+def main():
+    while True:
+        play_game()
+
+        play_again = input("\nMöchtest du noch einmal spielen? (ja/nein): ").lower()
+        if not play_again.startswith("j"):
+            break
+
+    print("\nDanke fürs Spielen! Auf Wiedersehen!")
+
+
+if __name__ == "__main__":
+    main()
